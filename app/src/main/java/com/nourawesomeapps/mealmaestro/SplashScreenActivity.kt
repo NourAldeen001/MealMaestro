769c6b797de080f939d34499aa882d827b491b1c
NourAldeen001 <118866955+NourAldeen001@users.noreply.github.com>
2024-08-23 18:07:39 +0300

    Add SplashScreen

--- app/src/main/java/com/nourawesomeapps/mealmaestro/SplashScreenActivity.kt
+++ app/src/main/java/com/nourawesomeapps/mealmaestro/SplashScreenActivity.kt
@@ -1,11 +1,49 @@
 package com.nourawesomeapps.mealmaestro
 
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.nourawesomeapps.mealmaestro.auth.AuthActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        initSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        goToAuth()
+    }
+
+    private fun initSplashScreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            installSplashScreen()
+
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+                val slideUp = ObjectAnimator.ofFloat(
+                    splashScreenView,
+                    View.TRANSLATION_Y,
+                    0f,
+                    -splashScreenView.height.toFloat()
+                )
+
+                slideUp.interpolator = AnticipateInterpolator()
+                slideUp.duration = 1000L
+
+                slideUp.doOnEnd { splashScreenView.remove() }
+
+                slideUp.start()
+            }
+        }
+    }
+
+    private fun goToAuth() {
+        startActivity(Intent(this, AuthActivity::class.java))
+        finish()
     }
 }
